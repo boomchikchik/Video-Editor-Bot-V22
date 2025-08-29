9b03e626736a962bed9ebb58579c21505a48cfd0
boomchikchik <bulkdeals99@gmail.com>
2025-08-29 19:03:37 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
+
+
+API_ID = int(os.getenv("API_ID", 13216322))
+API_HASH = os.getenv("API_HASH", ""15e5e632a8a0e52251ac8c3ccbe462c7"")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7335432995:AAE9LR-VEOb4fdfKn3DVxM_F8s7BRtRoMIc")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://MoviesV2:Qwerty_1234@cluster0.e3kg7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1002461289592))
